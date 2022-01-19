e2d2d6213d1013bf3d8e53f39c3505e1515e0a65
victorkennedys <victorkennedy@nowstudy.online>
2022-01-19 23:26:33 +0100

    got rig of 1 variable

--- lib/models/authentication_model.dart
+++ lib/models/authentication_model.dart
@@ -5,13 +5,13 @@ import 'package:flutter/material.dart';
 class AuthenticationModel {
   authenticateNumber(
       {required String verificationCode,
-      required TextEditingController authController,
+      required String inputCode,
       required bool newUser,
       required String phone}) async {
     var dogOwners = FirebaseFirestore.instance.collection('dog owners');
     await FirebaseAuth.instance
         .signInWithCredential(PhoneAuthProvider.credential(
-            verificationId: verificationCode, smsCode: authController.text))
+            verificationId: verificationCode, smsCode: inputCode))
         .then((response) async {
       if (response.user != null) {
         if (newUser == true) {
