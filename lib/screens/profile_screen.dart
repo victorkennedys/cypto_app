e13b3bc8ec40e7463317e309b7915430655767ec
victorkennedys <victorkennedy@nowstudy.online>
2022-01-20 10:53:50 +0100

    removed unecessary imports

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,10 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:woof/components/app_button.dart';
 import 'package:woof/components/nav_bar.dart';
-import 'package:woof/constants.dart';
 import 'package:woof/main.dart';
-import 'package:woof/screens/helper%20onboarding/helper_services.dart';
+import 'package:woof/screens/walker%20onboarding/walker_services.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
